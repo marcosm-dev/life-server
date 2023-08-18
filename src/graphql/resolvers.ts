f05b5a90ef35297f879459857586c9ecb8f81121
Marcos Marrero <60783560+marcosm-dev@users.noreply.github.com>
2023-08-18 16:21:43 +0100

    fix auth token

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -308,7 +308,7 @@ export const resolvers = {
 			}
 		},
     logoutUser: async(_: any, {}, { currentUser }) => {
-      const { token } = currentUser
+      const token = currentUser?.token
 
       if (!token) return new GraphQLError('No estás identificado')
       try {
