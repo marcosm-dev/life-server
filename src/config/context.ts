8d9214f3917a935d4e620e2e2071e4e77e424c54
Marcos Marrero <marcosa.mm@icloud.com>
2024-04-29 00:18:41 +0100

    Actualizar lógica de autenticación en createContext en context.ts

--- src/config/context.ts
+++ src/config/context.ts
@@ -14,17 +14,21 @@ export async function createContext(
   { request, params  }: YogaInitialContext
 ): Promise<GraphQLContext> {
   console.log(params)
-  // const isAuth = ['loginUser', 'signUp', 'recoveryPassword', 'resetPassword'].includes(params.operationName as string)
-  // // const isAuth = ['loginUser', 'signUp', 'recoveryPassword', 'resetPassword'].includes(params.operationName as string) || params.query?.includes('resetPassword') || params.query?.includes('loginUser') || params.query?.includes('signUp')
-  // if (isAuth) {
-  //   console.log('user: null')
-  //   return userContext
-  // }
-  // const token = request.headers.get('authorization')
-  // const userId = await decodeAuthHeader(token as string) as any
-  // if (!userId) throw new GraphQLError('unauthorized')
+  const isAuth = ['loginUser', 'signUp', 'recoveryPassword', 'resetPassword'].includes(params.operationName as string)
+  // const isAuth = ['loginUser', 'signUp', 'recoveryPassword', 'resetPassword'].includes(params.operationName as string) || params.query?.includes('resetPassword') || params.query?.includes('loginUser') || params.query?.includes('signUp')
+  if (isAuth) {
+    console.log('user: null')
+    return userContext
+  }
+  try {
+    const token = request.headers.get('authorization')
+    const userId = await decodeAuthHeader(token as string) as any
+    if (!userId) throw new GraphQLError('unauthorized')
+    userContext.userId = userId
+  } catch (error) {
+    console.log(error)
+  }
 
-  userContext.userId = "65a5d1636f849f3e3c8ade9c"
 
   return userContext
 }
