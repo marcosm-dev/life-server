3bc23c92ec09067788daaade77c9cc757fb9a437
Marcos Marrero <marcosa.mm@icloud.com>
2024-04-28 22:37:16 +0100

    open

--- src/config/context.ts
+++ src/config/context.ts
@@ -15,9 +15,9 @@ export async function createContext(
   { request, params  }: YogaInitialContext
 ): Promise<GraphQLContext> {
   console.log(params)
-  const isAuth = ['loginUser', 'signUp', 'recoveryPassword', 'resetPassword'].includes(params.operationName as string)
+  // const isAuth = ['loginUser', 'signUp', 'recoveryPassword', 'resetPassword'].includes(params.operationName as string)
   // const isAuth = ['loginUser', 'signUp', 'recoveryPassword', 'resetPassword'].includes(params.operationName as string) || params.query?.includes('resetPassword') || params.query?.includes('loginUser') || params.query?.includes('signUp')
-  if (isAuth) return userContext
+  // if (isAuth) return userContext
 
   const token = request.headers.get('authorization')
   const userId = await decodeAuthHeader(token as string) as any
