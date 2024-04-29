1593cb6040f677376cb868bb4f26bf76fff22833
Marcos Marrero <marcosa.mm@icloud.com>
2024-04-29 01:06:59 +0100

    Actualizar archivos de configuración y modelos

--- src/config/schema.ts
+++ src/config/schema.ts
@@ -3,13 +3,15 @@ import { createSchema } from 'graphql-yoga'
 import { mergeTypeDefs, mergeResolvers } from '@graphql-tools/merge'
 import { fileURLToPath } from 'url'
 import { dirname, join } from 'path'
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 const envExtension = process.env.NODE_ENV === 'production' ? 'js' : 'ts'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 async function buildSchema() {
   const typeDefsArray = loadFilesSync(join(__dirname, '../**/*.graphql'))
-  const resolversArray = await loadFiles(join(__dirname, '../**/*.resolver.' + 'js'))
+  const resolversArray = await loadFiles(join(__dirname, '../**/*.resolver.' + envExtension))
   const typeDefs = mergeTypeDefs(typeDefsArray)
   const resolvers = mergeResolvers(resolversArray)
   console.log(typeDefs)
